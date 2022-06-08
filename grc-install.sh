e176cf58c96e8beb657a4a630c7ab9b875cd1500
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-06-07 23:46:56 -0300

    Test 3.

--- grc-install.sh
+++ grc-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Place of installation.
-cd /usr/local
+# If any error occurs, exits the script.
+set -e
 
-# Creates hidden directory.
-mkdir .grc
-cd .grc
+# Creates GRC directory.
+mkdir GRC
+cd GRC
+echo "[INFO] Created folder 'GRC', please do not move this folder to another path."
 
 # Clones GRC repository.
 git clone https://github.com/ArthurSudbrackIbarra/GitHub-Repo-Creator.git
 cd GitHub-Repo-Creator
-
-exit 0
+echo "[INFO] Cloned GRC GitHub repository."
 
 # Giving all users the permission to execute grc script.
 chmod +x "$PWD/grc"
@@ -19,9 +19,6 @@ chmod +x "$PWD/grc"
 # Adding the project directory to $PATH.
 sed -i "/grc/d" ~/.bashrc
 echo "export PATH+=\":$PWD\" # grc" >> ~/.bashrc
+echo "[INFO] Added repository directory to your PATH."
 
-echo
-echo "You may close this terminal now for the changes to take effect."
-echo
-
-# bash <(curl -s http://mywebsite.com/myscript.txt)
+echo "[INFO] You may close this terminal now for the changes to take effect."
