3e6ae25afbbe6b296dc15dd3c6f8bf5bcc88f7e0
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-06-10 02:07:44 -0300

    Fixed version manager constructor.

--- .program-files/modules/version.py
+++ .program-files/modules/version.py
@@ -3,7 +3,7 @@ from time import time
 
 
 class VersionManager:
-    def __init__(self, daysInCache: int) -> None:
+    def __init__(self, daysInCache: float) -> None:
         self.versionPath = path.abspath(
             path.join(path.dirname(__file__), '../config/VERSION'))
         self.daysInCache = daysInCache if daysInCache > 0 else 1
