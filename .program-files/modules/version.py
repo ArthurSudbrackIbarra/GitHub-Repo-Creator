1e165daa67c0e64d647b92cfd28d5db05756b213
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-06-29 17:53:37 -0300

    Token and version not tracked anymore by git.

--- .program-files/modules/version.py
+++ .program-files/modules/version.py
@@ -5,19 +5,23 @@ from time import time
 class VersionManager:
     def __init__(self, daysInCache: float) -> None:
         self.versionPath = path.abspath(
-            path.join(path.dirname(__file__), '../config/VERSION'))
+            path.join(path.dirname(__file__), '../configurations/version.txt'))
         self.daysInCache = daysInCache if daysInCache > 0 else 1
 
     # readVersion is not used...
     def readVersion(self) -> str:
+        if not path.exists(self.versionPath):
+            return ""
         with open(self.versionPath, 'r') as versionFile:
             return versionFile.readline()
 
     def writeVersion(self, version: str) -> None:
-        with open(self.versionPath, 'w') as versionFile:
+        with open(self.versionPath, 'w+') as versionFile:
             versionFile.write(version)
 
     def shouldResetCache(self) -> bool:
+        if not path.exists(self.versionPath):
+            return False
         secsDiff = time() - path.getatime(self.versionPath)
         dayDiff = secsDiff / 86400
         return dayDiff >= self.daysInCache
