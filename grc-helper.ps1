cf85d213f826c87bc5a617641c093c9442fba323
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-07-06 17:52:29 -0300

    Fix grc helper.

--- grc-helper.ps1
+++ grc-helper.ps1
@@ -1,4 +1,6 @@
 Set-ExecutionPolicy Unrestricted -Scope Process
 Invoke-Expression $PSScriptRoot\venv\Scripts\Activate.ps1
 python $PSScriptRoot\.program-files\main.py $args
+$exitCode = $?
 deactivate
+exit $exitCode
